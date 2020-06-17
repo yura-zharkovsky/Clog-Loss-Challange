96d3ce96d4e2f2591ce07b26ad16a097a4261003
Yura Zharkovsky <yura@percepto.co>
2020-06-17 14:58:20 +0300

    Added option to extract masks from fames to template notebook

--- clog_utils.py
+++ clog_utils.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from IPython.display import HTML
 from base64 import b64encode
-
+from scipy.ndimage import binary_fill_holes
 
 def display_clip(path):  
     print(path)
@@ -29,3 +29,15 @@ def read_clip(path):
             break
     cap.release()
     return np.stack(frames, axis=0)
+
+
+def extract_mask(img, threshold=20):
+    img = img-np.mean(img, axis=-1)[...,None]
+    img = np.max(np.abs(img), axis=-1)
+    img = (img > threshold)
+    img = binary_fill_holes(img).astype(np.uint8)
+    return img[...,None]
+
+
+def extract_masks(imgs):
+    return [extract_mask(img) for img in imgs]
